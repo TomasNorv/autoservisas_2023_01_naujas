cc37dc484870f506a7df854a1a954d86e8db13ab
Donatas Noreika <donoras@gmail.com>
2023-01-17 08:50:39 +0200

    Modeliai registruoti admin.py faile

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -6,6 +6,9 @@ class AutomobilioModelis(models.Model):
     marke = models.CharField(verbose_name="Markė", max_length=100)
     modelis = models.CharField(verbose_name="Modelis", max_length=100)
 
+    def __str__(self):
+        return f"{self.marke} {self.modelis}"
+
 
 class Automobilis(models.Model):
     valstybinis_nr = models.CharField(verbose_name="Valstybinis numeris", max_length=100)
@@ -13,18 +16,28 @@ class Automobilis(models.Model):
     klientas = models.CharField(verbose_name="Klientas", max_length=100)
     automobilio_modelis = models.ForeignKey(to=AutomobilioModelis, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return f"{self.automobilio_modelis} ({self.valstybinis_nr})"
+
 
 class Paslauga(models.Model):
     pavadinimas = models.CharField(verbose_name="Pavadinimas", max_length=100)
     kaina = models.FloatField(verbose_name="Kaina")
 
+    def __str__(self):
+        return f"{self.pavadinimas}"
 
 class Uzsakymas(models.Model):
-    data = models.DateField(verbose_name="Data", auto_now_add=True)
+    data = models.DateTimeField(verbose_name="Data", auto_now_add=True)
     automobilis = models.ForeignKey(to="Automobilis", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.automobilis} ({self.data})"
 
 class UzsakymoEilute(models.Model):
     uzsakymas = models.ForeignKey(to="Uzsakymas", on_delete=models.CASCADE)
     paslauga = models.ForeignKey(to="Paslauga", on_delete=models.SET_NULL, null=True)
     kiekis = models.IntegerField(verbose_name="Kiekis")
+
+    def __str__(self):
+        return f"{self.uzsakymas.data}, {self.paslauga} ({self.kiekis})"
