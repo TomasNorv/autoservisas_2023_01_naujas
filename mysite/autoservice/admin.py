b8e79b21337c33e7afdf167af1f3a0ccdb7c1b29
Donatas Noreika <donoras@gmail.com>
2023-01-18 08:16:27 +0200

    3: +Padaryta, kad modelių sąrašuose būtų atvaizduojami reikalingi stulpeliai

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -12,11 +12,22 @@ class UzsakymoEiluteInLine(admin.TabularInline):
     model = UzsakymoEilute
     extra = 0
 
+
 class UzsakymasAdmin(admin.ModelAdmin):
+    list_display = ('automobilis', 'data')
     inlines = [UzsakymoEiluteInLine]
 
-admin.site.register(Automobilis)
+
+class AutomobilisAdmin(admin.ModelAdmin):
+    list_display = ('klientas', 'automobilio_modelis', 'valstybinis_nr', 'vin_kodas')
+
+
+class PaslaugaAdmin(admin.ModelAdmin):
+    list_display = ('pavadinimas', 'kaina')
+
+
+admin.site.register(Automobilis, AutomobilisAdmin)
 admin.site.register(AutomobilioModelis)
-admin.site.register(Paslauga)
+admin.site.register(Paslauga, PaslaugaAdmin)
 admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoEilute)
