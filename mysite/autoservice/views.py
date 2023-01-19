56411a1af992a0a6c70f76062f7d6fe199d0adbc
Donatas Noreika <donoras@gmail.com>
2023-01-19 08:31:24 +0200

    4: +Sukurti puslapį (ne admin), kuriame būtų matoma statistika: paslaugų kiekis, atliktų užsakymų kiekis, automobilių kiekis, +Susikurti savo puslapio stilių (base.html failą). Jei reikia, pridėkite css ir kitus failus (patartina naudoti bootstrap). Galite panaudoti paskaitoje rodytus pavyzdžius. Panaudokite frontend kurse išmoktas žinias! :)

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Paslauga, Automobilis, Uzsakymas
 
 # Create your views here.
+def index(request):
+    paslaugu_kiekis = Paslauga.objects.count()
+    automobiliu_kiekis = Automobilis.objects.count()
+    atliktu_uzsakymu_kiekis = Uzsakymas.objects.filter(status__exact='i').count()
+
+    kontekstas = {
+        "paslaugu_kiekis": paslaugu_kiekis,
+        "automobiliu_kiekis": automobiliu_kiekis,
+        "atliktu_uzsakymu_kiekis": atliktu_uzsakymu_kiekis,
+    }
+    return render(request, 'index.html', context=kontekstas)
