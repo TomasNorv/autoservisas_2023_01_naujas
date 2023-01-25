4096cb7fd8854dfe38282d199b546a79f425fbc7
Donatas Noreika <donoras@gmail.com>
2023-01-25 08:16:23 +0200

    7: +Susikonfiguruoti el. paštą slaptažodžio keitimui.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,6 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-k6txu56&$ivj*)jn(qzm!)sg&8r*s1$w_hnr)8a%c9&90%616h'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+# el. pašto adresas iš kurio siųsite
+EMAIL_HOST_PASSWORD = 'VerySecret'
+# slaptažodis
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
